54ed56972e68523ef344d6e9e119258630f74b77
ToeJammm <amarlow6@vols.utk.edu>
2024-02-15 00:14:34 +0000

    edited front-end a little

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,10 @@
-import React, { useEffect } from 'react'
+import "./App.css"
 
 function App() {
-  const [data, setData] = React.useState([]);
-  useEffect(() => {
-    fetch('http://localhost:8081/people')
-      .then(res => res.json())
-      .then(data => console.log(data))
-      .cach(err => console.log(err))
-  }
-  , [])
+  
   return (
-    <div style={{padding: "50px"}}>
-      <table>
-        <thead>
-          <th>ID</th>
-        </thead>
-        <tbody>
-          {data.map((d, i) => (
-            <tr key={i}>
-              <td>{d.id}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="container">
+      <h1 className="content">ClassMate</h1>
     </div>
   )
 }
