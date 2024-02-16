c5d920b4381570c30849f182e4f5de0cf92a1f76
Silverasdf <yhg461@vols.utk.edu>
2024-02-16 18:43:44 +0000

    Started mysql tutorial

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,6 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'test'
-});
-
 app.get('/', (re, res) => {
     return res.json("From Backend Side");
 });
