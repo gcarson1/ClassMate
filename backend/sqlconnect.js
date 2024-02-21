a0217df9259fc86e1a11a3ec5e1ee401e51eb665
Silverasdf <yhg461@vols.utk.edu>
2024-02-21 21:56:54 +0000

    Modified Test Query

--- backend/sqlconnect.js
+++ backend/sqlconnect.js
@@ -21,26 +21,23 @@ const config = {
 
 async function testQuery() {
     try {
-        var poolConnection = await sql.connect(config);
+        let poolConnection = await sql.connect(config);
 
         console.log("Reading rows from the Table...");
-        var resultSet = await poolConnection.request().query(`SELECT TOP 50 pc.Name as CategoryName,
-            p.name as ProductName 
-            FROM [SalesLT].[ProductCategory] pc
-            JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid`);
+        let resultSet = await poolConnection.request().query(`SELECT * FROM [dbo].[Dummy]`);
 
         console.log(`${resultSet.recordset.length} rows returned.`);
 
         // output column headers
-        var columns = "";
-        for (var column in resultSet.recordset.columns) {
+        let columns = "";
+        for (let column in resultSet.recordset.columns) {
             columns += column + ", ";
         }
         console.log("%s\t", columns.substring(0, columns.length - 2));
 
         // ouput row contents from default record set
         resultSet.recordset.forEach(row => {
-            console.log("%s\t%s", row.CategoryName, row.ProductName);
+            console.log("%s   %s   %s", row.ID, row.Name, row.Age);
         });
 
         // close connection only when we're certain application is finished
