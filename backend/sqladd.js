0a7d26ea5db15741b36b1d79eb576123c9aa3d2b
Silverasdf <yhg461@vols.utk.edu>
2024-03-01 12:43:00 -0500

    Started work on delete functions

--- backend/sqladd.js
+++ backend/sqladd.js
@@ -9,6 +9,9 @@ export async function addUser(poolConnection, username, password, email, uniID) 
         let resultSet = await poolConnection.request().query(
             
             `
+            -- Begin a transaction to ensure atomicity
+            BEGIN TRANSACTION;
+
             -- Step 1: Determine the highest UserID currently in the table
             DECLARE @maxUserID INT;
             SELECT @maxUserID = MAX(UserID) FROM Users;
@@ -18,7 +21,7 @@ export async function addUser(poolConnection, username, password, email, uniID) 
 
             -- Step 3: Insert the new user into the table
             INSERT INTO [dbo].[User] (UserID, Username, Password, Email, UniID) 
-            VALUES ('@maxUserID, ${username}', '${password}', '${email}', '${uniID}');`
+            VALUES (@maxUserID, '${username}', '${password}', '${email}', ${uniID});`
         );
         return resultSet.recordset;
     } catch (err) {
@@ -33,12 +36,16 @@ export async function addUniversity(poolConnection, name) {
         console.log("Adding university to database");
         let resultSet = await poolConnection.request().query(
             `
+            -- Begin a transaction to ensure atomicity
+            BEGIN TRANSACTION;
+
             DECLARE @maxUniID INT;
             SELECT @maxUniID = MAX(UniID) FROM University;
             SET @maxUniID = ISNULL(@maxUniID, 0) + 1;
 
             INSERT INTO [dbo].[University] (UniID, UniName) 
-            VALUES ('@maxUniID, ${name});`
+            VALUES (@maxUniID, '${name}');
+            `
         );
         return resultSet.recordset;
     } catch (err) {
@@ -47,10 +54,26 @@ export async function addUniversity(poolConnection, name) {
     }
 }
 
-export async function addClass(poolConnection, name, universityID) {
+export async function addClass(poolConnection, className, classNum, classTypeID) {
+    //TODO: Test function
     try {
         console.log("Adding class to database");
-        let resultSet = await poolConnection.request().query(`INSERT INTO [dbo].[Course] (Name, UniversityID) VALUES ('${name}', '${universityID}')`);
+        let resultSet = await poolConnection.request().query(`
+        -- Begin a transaction to ensure atomicity
+        BEGIN TRANSACTION;
+
+        DECLARE @maxID INT;
+        SELECT @maxID = MAX(ClassID) FROM Class;
+        SET @maxID = ISNULL(@maxID, 0) + 1;
+
+        INSERT INTO [dbo].[Class] (ClassID, ClassName, ClassNum, ClassTypeID) 
+        VALUES (@maxID, '${className}', ${classNum}, ${classTypeID});)
+        
+        -- Junction table ClassType_Class
+
+        INSERT INTO [dbo].[ClassType_Class] (ClassTypeID, ClassID)
+        VALUES (${classTypeID}, @maxID);
+        `);
         return resultSet.recordset;
     } catch (err) {
         console.error(err.message);
@@ -58,10 +81,46 @@ export async function addClass(poolConnection, name, universityID) {
     }
 }
 
-export async function addComment(poolConnection, userID, courseID, rating, review) {
+export async function addComment(poolConnection, userID, comment, termTaken, grade, classID, difficultyValue, qualityValue, professorID) {
     try {
         console.log("Adding comment to database");
-        let resultSet = await poolConnection.request().query(`INSERT INTO [dbo].[Review] (UserID, CourseID, Rating, Review) VALUES ('${userID}', '${courseID}', '${rating}', '${review}')`);
+        let resultSet = await poolConnection.request().query(`
+        -- Begin a transaction to ensure atomicity
+        BEGIN TRANSACTION;
+
+        -- Adding the new difficulty first
+        DECLARE @maxID INT;
+        SELECT @maxID = MAX(DifficultyID) FROM Difficulty;
+        SET @maxID = ISNULL(@maxID, 0) + 1;
+
+        INSERT INTO [dbo].[Difficulty] (DifficultyID, DifficultyValue, ProfessorID, UserID, ClassID, QualityValue) 
+        VALUES (@maxID, ${difficultyValue}, ${professorID}, ${userID}, ${classID}, ${qualityValue});
+
+        -- Junction table Class_Professor
+
+        INSERT INTO [dbo].[Class_Professor] (ClassID, ProfessorID)
+        VALUES (${classID}, ${professorID});
+
+        -- Junction table User_Difficulty
+
+        INSERT INTO [dbo].[User_Difficulty] (UserID, DifficultyID)
+        VALUES (${userID}, @maxID);
+
+        -- Junction table Class_Difficulty
+
+        INSERT INTO [dbo].[Class_Difficulty] (ClassID, DifficultyID)
+        VALUES (${classID}, @maxID);
+
+        -- Now adding the comment
+
+        DECLARE @maxCID INT;
+        SELECT @maxCID = MAX(CommentID) FROM Comments;
+        SET @maxCID = ISNULL(@maxID, 0) + 1;
+
+        INSERT INTO [dbo].[Comments] (Comment, TermTaken, Grade, CommentID, UserID, ClassID, DifficultyID, PostDate) 
+        VALUES ('${comment}', '${termTaken}', '${grade}', @maxCID, ${userID}, ${classID}, @maxID, CURRENT_TIMESTAMP);
+
+        `);
         return resultSet.recordset;
     } catch (err) {
         console.error(err.message);
@@ -75,12 +134,15 @@ export async function addProfessor(poolConnection, name, universityID) {
         console.log("Adding professor to database");
         let resultSet = await poolConnection.request().query(
             `
+            -- Begin a transaction to ensure atomicity
+            BEGIN TRANSACTION;
+
             DECLARE @maxID INT;
             SELECT @maxID = MAX(ProfessorID) FROM Professors;
             SET @maxID = ISNULL(@maxID, 0) + 1;
 
             INSERT INTO [dbo].[Professors] (ProfessorID, Name, UniID) 
-            VALUES ('@maxID, ${name}, ${universityID});`
+            VALUES (@maxID, '${name}', ${universityID});`
         );
         return resultSet.recordset;
     } catch (err) {
@@ -95,12 +157,15 @@ export async function addClassType(poolConnection, name, universityID) {
         console.log("Adding class type to database");
         let resultSet = await poolConnection.request().query(
             `
+            -- Begin a transaction to ensure atomicity
+            BEGIN TRANSACTION;
+
             DECLARE @maxID INT;
             SELECT @maxID = MAX(ClassTypeID) FROM ClassType;
             SET @maxID = ISNULL(@maxID, 0) + 1;
 
             INSERT INTO [dbo].[ClassType] (ClassTypeID, ClassType, UniID) 
-            VALUES ('@maxID, ${name}, ${universityID});`
+            VALUES (@maxID, '${name}', ${universityID});`
         );
         return resultSet.recordset;
     } catch (err) {
@@ -109,10 +174,37 @@ export async function addClassType(poolConnection, name, universityID) {
     }
 }
 
-export async function addDifficulty(poolConnection, name) {
+export async function addDifficulty(poolConnection, difficultyValue, qualityValue, userID, classID, professorID) {
+    //TODO: Test function
     try {
         console.log("Adding Difficulty to database");
-        let resultSet = await poolConnection.request().query(`INSERT INTO [dbo].[CourseType] (Name) VALUES ('${name}')`);
+        let resultSet = await poolConnection.request().query(`
+        -- Begin a transaction to ensure atomicity
+        BEGIN TRANSACTION;
+        
+        DECLARE @maxID INT;
+        SELECT @maxID = MAX(DifficultyID) FROM Difficulty;
+        SET @maxID = ISNULL(@maxID, 0) + 1;
+
+        INSERT INTO [dbo].[Difficulty] (DifficultyID, DifficultyValue, ProfessorID, UserID, ClassID, QualityValue) 
+        VALUES (@maxID, ${difficultyValue}, ${professorID}, ${userID}, ${classID}, ${qualityValue});
+
+        -- Junction table Class_Professor
+
+        INSERT INTO [dbo].[Class_Professor] (ClassID, ProfessorID)
+        VALUES (${classID}, ${professorID});
+
+        -- Junction table User_Difficulty
+
+        INSERT INTO [dbo].[User_Difficulty] (UserID, DifficultyID)
+        VALUES (${userID}, @maxID);
+
+        -- Junction table Class_Difficulty
+
+        INSERT INTO [dbo].[Class_Difficulty] (ClassID, DifficultyID)
+        VALUES (${classID}, @maxID);
+
+        `);
         return resultSet.recordset;
     } catch (err) {
         console.error(err.message);
--- backend/sqldelete.js
+++ backend/sqldelete.js
@@ -1,3 +1,184 @@
 //Contains all of the functions for deleting data from the database
+//TODO: Write a tree of dependencies to check
 
-import { closeConnection, connect } from './sqlconnect.js';
+//! None of these are written yet.
+export async function deleteUser(poolConnection, userID) {
+    //TODO: Test function
+    try {
+        console.log("Deleting user" + userID + "from database");
+        let resultSet = await poolConnection.request().query(`
+        BEGIN TRANSACTION;
+
+        -- Start by deleting all of the junction tables that reference something that will be deleted (difficulty, comment)
+
+        DELETE FROM [dbo].[Class_Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Difficulty] WHERE UserID = ${userID});
+        DELETE FROM [dbo].[Class_Professor] WHERE ProfessorID IN (SELECT ProfessorID FROM [dbo].[Professors] WHERE UserID = ${userID});
+
+        DELETE FROM [dbo].[Users] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[User_Difficulty] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[User_Comments] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[Comments] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[Difficulty] WHERE UserID = ${userID};
+        `);
+        return resultSet.recordset;
+    } catch (err) {
+        console.error(err.message);
+        return null;
+    }
+}
+
+//!Warning: VERY DANGEROUS FUNCTION -- YOU PROBABLY DON'T WANT TO USE THIS UNLESS YOU'RE USING TEST DATA
+export async function deleteUniversity(poolConnection, uniID) {
+    //TODO: Test function
+    try {
+        console.log("Deleting University" + uniID + "from database");
+        let resultSet = await poolConnection.request().query(`
+        BEGIN TRANSACTION;
+
+        -- Start by deleting all of the tables that reference something that will be deleted
+
+        DELETE FROM [dbo].[Class_Comments] WHERE ClassID IN (SELECT ClassID FROM [dbo].[Class] WHERE UniID = ${uniID});
+        DELETE FROM [dbo].[ClassType_Class] WHERE ClassTypeID IN (SELECT ClassTypeID FROM [dbo].[ClassType] WHERE UniID = ${uniID});
+        DELETE FROM [dbo].[Class_Difficulty] WHERE ClassID IN (SELECT ClassID FROM [dbo].[Class] WHERE UniID = ${uniID});
+        DELETE FROM [dbo].[Class_Professors] WHERE ClassID IN (SELECT ClassID FROM [dbo].[Class] WHERE UniID = ${uniID});
+        DELETE FROM [dbo].[Difficulty] WHERE ClassID IN (SELECT ClassID FROM [dbo].[Class] WHERE UniID = ${uniID});
+        DELETE FROM [dbo].[Comments] WHERE ClassID IN (SELECT ClassID FROM [dbo].[Class] WHERE UniID = ${uniID});
+        DELETE FROM [dbo].[User_Comments] WHERE UserID IN (SELECT UserID FROM [dbo].[Users] WHERE UniID = ${uniID});
+        DELETE FROM [dbo].[User_Difficulty] WHERE UserID IN (SELECT UserID FROM [dbo].[Users] WHERE UniID = ${uniID});
+        DELETE FROM [dbo].[Users] WHERE UniID = ${uniID};
+
+        DELETE FROM [dbo].[ClassType] WHERE UniID = ${uniID};
+        DELETE FROM [dbo].[Professors] WHERE UniID = ${uniID};
+        DELETE FROM [dbo].[Class] WHERE UniID = ${uniID};
+        DELETE FROM [dbo].[University] WHERE UniID = ${uniID};
+        `);
+        return resultSet.recordset;
+    } catch (err) {
+        console.error(err.message);
+        return null;
+    }
+}
+
+export async function deleteClass(poolConnection, classID) {
+    //TODO: Test function
+    try {
+        console.log("Deleting class" + classID + "from database");
+        let resultSet = await poolConnection.request().query(`
+        BEGIN TRANSACTION;
+
+        -- Start by deleting all of the junction tables that reference something that will be deleted (difficulty, comment)
+
+        DELETE FROM [dbo].[Class_Difficulty] WHERE ClassID = ${classID};
+        DELETE FROM [dbo].[Class_Professors] WHERE ClassID = ${classID};
+        DELETE FROM [dbo].[Class_Comments] WHERE ClassID = ${classID};
+        DELETE FROM [dbo].[ClassType_Class] WHERE ClassID = ${classID};
+        DELETE FROM [dbo].[User_Comments] WHERE CommentID IN (SELECT CommentID FROM [dbo].[Comments] WHERE ClassID = ${classID});
+        DELETE FROM [dbo].[User_Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Difficulty] WHERE ClassID = ${classID});
+
+        DELETE FROM [dbo].[Difficulty] WHERE ClassID = ${classID};
+        DELETE FROM [dbo].[Comments] WHERE ClassID = ${classID};
+        `);
+        return resultSet.recordset;
+    } catch (err) {
+        console.error(err.message);
+        return null;
+    }
+}
+
+export async function deleteComment(poolConnection, commentID) {
+    try {
+        console.log("Deleting comment" + commentID + "from database");
+        let resultSet = await poolConnection.request().query(`
+        BEGIN TRANSACTION;
+
+        -- Start by deleting all of the junction tables that reference something that will be deleted (difficulty, comment)
+        DELETE FROM [dbo].[User_Comments] WHERE CommentID = ${commentID};
+        DELETE FROM [dbo].[Class_Comments] WHERE CommentID = ${commentID};
+        DELETE FROM [dbo].[User_Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Comments] WHERE CommentID = ${commentID});
+        DELETE FROM [dbo].[Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Comments] WHERE CommentID = ${commentID});)
+        DELETE FROM [dbo].[Class_Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Comments] WHERE CommentID = ${commentID});
+
+        DELETE FROM [dbo].[Comments] WHERE CommentID = ${commentID};
+        `);
+        return resultSet.recordset;
+    } catch (err) {
+        console.error(err.message);
+        return null;
+    }
+}
+
+export async function deleteProfessor(poolConnection, name, universityID) {
+    //TODO: Test function
+    try {
+        console.log("Deleting user" + userID + "from database");
+        let resultSet = await poolConnection.request().query(`
+        BEGIN TRANSACTION;
+
+        -- Start by deleting all of the junction tables that reference something that will be deleted (difficulty, comment)
+
+        DELETE FROM [dbo].[Class_Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Difficulty] WHERE UserID = ${userID});
+        DELETE FROM [dbo].[Class_Professor] WHERE ProfessorID IN (SELECT ProfessorID FROM [dbo].[Professors] WHERE UserID = ${userID});
+
+        DELETE FROM [dbo].[Users] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[User_Difficulty] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[User_Comments] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[Comments] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[Difficulty] WHERE UserID = ${userID};
+        `);
+        return resultSet.recordset;
+    } catch (err) {
+        console.error(err.message);
+        return null;
+    }
+}
+
+export async function deleteClassType(poolConnection, name, universityID) {
+    //TODO: Test function
+    try {
+        console.log("Deleting user" + userID + "from database");
+        let resultSet = await poolConnection.request().query(`
+        BEGIN TRANSACTION;
+
+        -- Start by deleting all of the junction tables that reference something that will be deleted (difficulty, comment)
+
+        DELETE FROM [dbo].[Class_Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Difficulty] WHERE UserID = ${userID});
+        DELETE FROM [dbo].[Class_Professor] WHERE ProfessorID IN (SELECT ProfessorID FROM [dbo].[Professors] WHERE UserID = ${userID});
+
+        DELETE FROM [dbo].[Users] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[User_Difficulty] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[User_Comments] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[Comments] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[Difficulty] WHERE UserID = ${userID};
+        `);
+        return resultSet.recordset;
+    } catch (err) {
+        console.error(err.message);
+        return null;
+    }
+}
+
+export async function deleteDifficulty(poolConnection, difficultyValue, qualityValue, userID, classID, professorID) {
+    //TODO: Test function
+    try {
+        console.log("Deleting user" + userID + "from database");
+        let resultSet = await poolConnection.request().query(`
+        BEGIN TRANSACTION;
+
+        -- Start by deleting all of the junction tables that reference something that will be deleted (difficulty, comment)
+
+        DELETE FROM [dbo].[Class_Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Difficulty] WHERE UserID = ${userID});
+        DELETE FROM [dbo].[Class_Professor] WHERE ProfessorID IN (SELECT ProfessorID FROM [dbo].[Professors] WHERE UserID = ${userID});
+
+        DELETE FROM [dbo].[Users] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[User_Difficulty] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[User_Comments] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[Comments] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[Difficulty] WHERE UserID = ${userID};
+        `);
+        return resultSet.recordset;
+    } catch (err) {
+        console.error(err.message);
+        return null;
+    }
+}
+
