ee9b63346dc6504bb367150a2b8af4ff438b0af2
Silverasdf <yhg461@vols.utk.edu>
2024-03-01 13:44:17 -0500

    Wrote all add/delete functions -- HAVE YET TO TEST

--- backend/sqldelete.js
+++ backend/sqldelete.js
@@ -1,7 +1,6 @@
 //Contains all of the functions for deleting data from the database
 //TODO: Write a tree of dependencies to check
 
-//! None of these are written yet.
 export async function deleteUser(poolConnection, userID) {
     //TODO: Test function
     try {
@@ -77,6 +76,7 @@ export async function deleteClass(poolConnection, classID) {
 
         DELETE FROM [dbo].[Difficulty] WHERE ClassID = ${classID};
         DELETE FROM [dbo].[Comments] WHERE ClassID = ${classID};
+        DELETE FROM [dbo].[Class] WHERE ClassID = ${classID};
         `);
         return resultSet.recordset;
     } catch (err) {
@@ -95,7 +95,7 @@ export async function deleteComment(poolConnection, commentID) {
         DELETE FROM [dbo].[User_Comments] WHERE CommentID = ${commentID};
         DELETE FROM [dbo].[Class_Comments] WHERE CommentID = ${commentID};
         DELETE FROM [dbo].[User_Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Comments] WHERE CommentID = ${commentID});
-        DELETE FROM [dbo].[Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Comments] WHERE CommentID = ${commentID});)
+        DELETE FROM [dbo].[Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Comments] WHERE CommentID = ${commentID});
         DELETE FROM [dbo].[Class_Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Comments] WHERE CommentID = ${commentID});
 
         DELETE FROM [dbo].[Comments] WHERE CommentID = ${commentID};
@@ -107,23 +107,23 @@ export async function deleteComment(poolConnection, commentID) {
     }
 }
 
-export async function deleteProfessor(poolConnection, name, universityID) {
+export async function deleteProfessor(poolConnection, professorID) {
     //TODO: Test function
     try {
-        console.log("Deleting user" + userID + "from database");
+        console.log("Deleting professor" + professorID + "from database");
         let resultSet = await poolConnection.request().query(`
         BEGIN TRANSACTION;
 
         -- Start by deleting all of the junction tables that reference something that will be deleted (difficulty, comment)
-
-        DELETE FROM [dbo].[Class_Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Difficulty] WHERE UserID = ${userID});
-        DELETE FROM [dbo].[Class_Professor] WHERE ProfessorID IN (SELECT ProfessorID FROM [dbo].[Professors] WHERE UserID = ${userID});
-
-        DELETE FROM [dbo].[Users] WHERE UserID = ${userID};
-        DELETE FROM [dbo].[User_Difficulty] WHERE UserID = ${userID};
-        DELETE FROM [dbo].[User_Comments] WHERE UserID = ${userID};
-        DELETE FROM [dbo].[Comments] WHERE UserID = ${userID};
-        DELETE FROM [dbo].[Difficulty] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[Class_Professors] WHERE ProfessorID = ${professorID};
+        DELETE FROM [dbo].[Class_Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Difficulty] WHERE ProfessorID = ${professorID});
+        DELETE FROM [dbo].[User_Comments] WHERE CommentID IN (SELECT CommentID FROM [dbo].[Comments] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Difficulty] WHERE ProfessorID = ${professorID}));
+        DELETE FROM [dbo].[Class_Comments] WHERE CommentID IN (SELECT CommentID FROM [dbo].[Comments] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Difficulty] WHERE ProfessorID = ${professorID}));
+        DELETE FROM [dbo].[Comments] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Difficulty] WHERE ProfessorID = ${professorID});
+        DELETE FROM [dbo].[User_Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Difficulty] WHERE ProfessorID = ${professorID});
+        
+        DELETE FROM [dbo].[Difficulty] WHERE ProfessorID = ${professorID};
+        DELETE FROM [dbo].[Professors] WHERE ProfessorID = ${professorID};
         `);
         return resultSet.recordset;
     } catch (err) {
@@ -132,23 +132,28 @@ export async function deleteProfessor(poolConnection, name, universityID) {
     }
 }
 
-export async function deleteClassType(poolConnection, name, universityID) {
+export async function deleteClassType(poolConnection, classTypeID) {
     //TODO: Test function
     try {
-        console.log("Deleting user" + userID + "from database");
+        console.log("Deleting user" + classTypeID + "from database");
         let resultSet = await poolConnection.request().query(`
         BEGIN TRANSACTION;
 
         -- Start by deleting all of the junction tables that reference something that will be deleted (difficulty, comment)
 
-        DELETE FROM [dbo].[Class_Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Difficulty] WHERE UserID = ${userID});
-        DELETE FROM [dbo].[Class_Professor] WHERE ProfessorID IN (SELECT ProfessorID FROM [dbo].[Professors] WHERE UserID = ${userID});
+        DELETE FROM [dbo].[User_Comments] WHERE CommentID IN (SELECT CommentID FROM [dbo].[Comments] WHERE ClassID IN (SELECT ClassID FROM [dbo].[Class] WHERE ClassTypeID = ${classTypeID}));
+        DELETE FROM [dbo].[User_Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Difficulty] WHERE ClassID IN (SELECT ClassID FROM [dbo].[Class] WHERE ClassTypeID = ${classTypeID}));
 
-        DELETE FROM [dbo].[Users] WHERE UserID = ${userID};
-        DELETE FROM [dbo].[User_Difficulty] WHERE UserID = ${userID};
-        DELETE FROM [dbo].[User_Comments] WHERE UserID = ${userID};
-        DELETE FROM [dbo].[Comments] WHERE UserID = ${userID};
-        DELETE FROM [dbo].[Difficulty] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[Class_Difficulty] WHERE ClassID IN (SELECT ClassID FROM [dbo].[Class] WHERE ClassTypeID = ${classTypeID});
+        DELETE FROM [dbo].[Class_Professors] WHERE ClassID IN (SELECT ClassID FROM [dbo].[Class] WHERE ClassTypeID = ${classTypeID});
+        DELETE FROM [dbo].[Class_Comments] ClassID IN (SELECT ClassID FROM [dbo].[Class] WHERE ClassTypeID = ${classTypeID});
+        DELETE FROM [dbo].[ClassType_Class] WHERE ClassTypeID = ${classTypeID};
+
+        DELETE FROM [dbo].[Difficulty] WHERE ClassID IN (SELECT ClassID FROM [dbo].[Class] WHERE ClassTypeID = ${classTypeID});
+        DELETE FROM [dbo].[Comments] WHERE ClassID IN (SELECT ClassID FROM [dbo].[Class] WHERE ClassTypeID = ${classTypeID});
+        DELETE FROM [dbo].[Class] WHERE ClassTypeID = ${classTypeID};
+
+        DELETE FROM [dbo].[ClassType] WHERE ClassTypeID = ${classTypeID};
         `);
         return resultSet.recordset;
     } catch (err) {
@@ -157,23 +162,22 @@ export async function deleteClassType(poolConnection, name, universityID) {
     }
 }
 
-export async function deleteDifficulty(poolConnection, difficultyValue, qualityValue, userID, classID, professorID) {
+export async function deleteDifficulty(poolConnection, difficultyID) {
     //TODO: Test function
     try {
-        console.log("Deleting user" + userID + "from database");
+        console.log("Deleting difficulty" + difficultyID + "from database");
         let resultSet = await poolConnection.request().query(`
         BEGIN TRANSACTION;
 
         -- Start by deleting all of the junction tables that reference something that will be deleted (difficulty, comment)
+        DELETE FROM [dbo].[User_Comments] WHERE CommentID IN (SELECT CommentID FROM [dbo].[Comments] WHERE DifficultyID = ${difficultyID});
+        DELETE FROM [dbo].[Class_Comments] WHERE CommentID IN (SELECT CommentID FROM [dbo].[Comments] WHERE DifficultyID = ${difficultyID});
 
-        DELETE FROM [dbo].[Class_Difficulty] WHERE DifficultyID IN (SELECT DifficultyID FROM [dbo].[Difficulty] WHERE UserID = ${userID});
-        DELETE FROM [dbo].[Class_Professor] WHERE ProfessorID IN (SELECT ProfessorID FROM [dbo].[Professors] WHERE UserID = ${userID});
-
-        DELETE FROM [dbo].[Users] WHERE UserID = ${userID};
-        DELETE FROM [dbo].[User_Difficulty] WHERE UserID = ${userID};
-        DELETE FROM [dbo].[User_Comments] WHERE UserID = ${userID};
-        DELETE FROM [dbo].[Comments] WHERE UserID = ${userID};
-        DELETE FROM [dbo].[Difficulty] WHERE UserID = ${userID};
+        DELETE FROM [dbo].[Class_Difficulty] WHERE DifficultyID = ${difficultyID};
+        DELETE FROM [dbo].[User_Difficulty] WHERE DifficultyID = ${difficultyID};
+        DELETE FROM [dbo].[Comments] WHERE DifficultyID = ${difficultyID};
+        
+        DELETE FROM [dbo].[Difficulty] WHERE DifficultyID = ${difficultyID};
         `);
         return resultSet.recordset;
     } catch (err) {
